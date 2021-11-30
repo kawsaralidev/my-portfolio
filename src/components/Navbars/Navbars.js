2ea1d10ed7bea2db35d093c14f9c534a3869f263
kawsaralidev <mdkawsarali346@gmail.com>
2021-11-30 23:32:18 +0600

    done

--- src/components/Navbars/Navbars.js
+++ src/components/Navbars/Navbars.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 const Navbars = () => {
     return (
         <>
-            <Navbar bg="light" variant="light" sticky="top" collapseOnSelect expand="lg">
+            <Navbar bg="dark" variant="light" sticky="top" collapseOnSelect expand="lg">
                 <Container >
 
                     <Navbar.Brand to="#home"> <span className="text-info fs-3">Ali Portfolio</span></Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/myprojects">My Projects</Nav.Link>
-                        <Nav.Link as={Link} to="/aboutme">About Me</Nav.Link>
-                        <Nav.Link as={Link} to="/contact">Contact Me</Nav.Link>
-                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        <Nav.Link className="text-white fs-5" as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link className="text-white fs-5" as={Link} to="/myprojects">My Projects</Nav.Link>
+                        <Nav.Link className="text-white fs-5" as={Link} to="/aboutme">About Me</Nav.Link>
+                        <Nav.Link className="text-white fs-5" as={Link} to="/contact">Contact Me</Nav.Link>
+                        <Nav.Link className="text-white fs-5" as={Link} to="/blog">Blog</Nav.Link>
                         
                     </Navbar.Collapse>
                    
